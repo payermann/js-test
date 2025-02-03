a4aae610c0a5b2b123f6f905a38d782a2c4458d5
Felix Fischer <payermann@gmail.com>
2025-02-03 15:45:41 +0100

    Fix greets

--- greets.js
+++ greets.js
@@ -1,5 +1,5 @@
 export function greet(name, age, car) {
-  console.log(`Hello ${name}!`);
-  console.log(`You are ${age} years old.`);
-  console.log(`Your favorite car is ${car}.`);
+  console.log(`Hallo ${name}!`);
+  console.log(`Sie sind ${age} Jahre alt.`);
+  console.log(`Ihr Lieblingsauto ist ${car}.`);
 }
