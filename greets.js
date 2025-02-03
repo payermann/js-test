89f96a70d77fb127593f26682789f095ef06cd88
Felix Fischer <payermann@gmail.com>
2025-02-03 12:06:29 +0100

    Add extra modul

---
+++ greets.js
@@ -0,0 +1,5 @@
+export function greet(name, age, car) {
+  console.log(`Hello ${name}!`);
+  console.log(`You are ${age} years old.`);
+  console.log(`Your favorite car is ${car}.`);
+}
--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import { question, questionInt, keyInSelect } from "readline-sync";
+import { greet } from "./greets.js";
 
 const name = question("What is your name? ");
 const age = questionInt("What is your age? ");
@@ -6,6 +7,4 @@ const age = questionInt("What is your age? ");
 const cars = ["Toyota", "Chevrolet", "Ford", "Nissan", "Honda"];
 const car = keyInSelect(cars, "What is your favorite car?");
 
-console.log(`Hello ${name}!`);
-console.log(`You are ${age} years old.`);
-console.log(`Your favorite car is ${cars[car]}.`);
+greet(name, age, cars[car]);
