d1af87e5856598b7aa85608b8c2c5139414c57b2
Felix Fischer <payermann@gmail.com>
2025-02-03 11:41:23 +0100

    Add andere Fragen

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
-import { question } from "readline-sync";
+import { question, questionInt, keyInSelect } from "readline-sync";
 
 const name = question("What is your name? ");
-const age = question("What is your age? ");
+const age = questionInt("What is your age? ");
+
+const cars = ["Toyota", "Chevrolet", "Ford", "Nissan", "Honda"];
+const car = keyInSelect(cars, "What is your favorite car?");
 
 console.log(`Hello ${name}!`);
 console.log(`You are ${age} years old.`);
+console.log(`Your favorite car is ${cars[car]}.`);
